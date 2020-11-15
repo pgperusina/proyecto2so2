7a9d651bb2c7ec4df8633b7dc7613d409a3dee10
Pablo G. Perusina <pablo.garcia@conexiogroup.com>
2020-11-14 21:08:21 -0600

    Se agregó observabilidad a serviores Python.

--- namespace-project/gloo-branch/pythonServer/pythonConsumer.py
+++ namespace-project/gloo-branch/pythonServer/pythonConsumer.py
@@ -3,6 +3,33 @@ from pymongo import MongoClient
 import redis
 import json
 
+from opentelemetry import trace
+from opentelemetry.ext import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+
+trace.set_tracer_provider(TracerProvider())
+
+# create a JaegerSpanExporter
+jaeger_exporter = jaeger.JaegerSpanExporter(
+    service_name='python-rabbitmq-consumer',
+    # configure agent
+    agent_host_name='jaeger-agent.observability.svc.cluster.local',
+    agent_port=6831,
+    # optional: configure also collector
+    # collector_host_name='localhost',
+    # collector_port=14268,
+    # collector_endpoint='/api/traces?format=jaeger.thrift',
+    # username=xxxx, # optional
+    # password=xxxx, # optional
+)
+
+# Create a BatchExportSpanProcessor and add the exporter to it
+span_processor = BatchExportSpanProcessor(jaeger_exporter)
+
+# add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 redis_id = 1
 KEY_INDEX = 'index'
 
@@ -19,6 +46,7 @@ except Exception as e:
 
 
 def consume_queue():
+    print("tracing consume_rabbitmq_queue")
     credentials = pika.PlainCredentials('guest', 'guest')
     parameters = pika.ConnectionParameters("34.72.226.148", 5672)
     connection = pika.BlockingConnection(parameters)
@@ -29,35 +57,46 @@ def consume_queue():
     channel.start_consuming()
 
 def persistInDB(ch, method, properties, body):
-    print("Covid19 case received: %r" % body)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("db-persist") as persist_in_db_span:
+        print("tracing persistInDB")
 
-    try:
-        data = json.loads(body)
-    except Exception as e:
-        print("Error deserializing message - %r", body)
-        print(e)
-    insert_mongo_db(data)
-    insert_redis(body)
+        print("Covid19 case received: " % body)
+
+        try:
+            data = json.loads(body)
+        except Exception as e:
+            print("Error deserializing message - %r", body)
+            print(e)
+        insert_mongo_db(data)
+        insert_redis(body)
 
 def insert_mongo_db(data):
-    try:
-        print("***** Insertando caso a Mongo DB *****")
-        mongo_db.casos.insert_one(data)
-        print("***** Caso insertado en MongoDB: ", data)
-    except Exception as e:
-        print("Error posting document to mongo DB")
-        print(e)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("insert_mongo_db") as insert_mongo_db_span:
+        print("tracing insert_mongo_db")
+        try:
+            print("***** Insertando caso a Mongo DB *****")
+            mongo_db.casos.insert_one(data)
+            print("***** Caso insertado en MongoDB: ", data)
+        except Exception as e:
+            print("Error posting document to mongo DB")
+            print(e)
+
 
 def insert_redis(data):
-    try:
-        print("***** Insertando caso a Redis *****")
-        redis_db.incr(KEY_INDEX, 1)
-        index = redis_db.get(KEY_INDEX).decode('utf-8')
-        int_index = int(index) 
-        redis_db.set(str(int_index), data)
-        print("***** Caso insertado en Redis: ", data)
-    except Exception as e:
-        print("Error posting document to Redis")
-        print(e)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("insert_redis") as insert_redis_span:
+        print("tracing insert_redis")
+        try:
+            print("***** Insertando caso a Redis *****")
+            redis_db.incr(KEY_INDEX, 1)
+            index = redis_db.get(KEY_INDEX).decode('utf-8')
+            int_index = int(index) 
+            redis_db.set(str(int_index), data)
+            print("***** Caso insertado en Redis: ", data)
+        except Exception as e:
+            print("Error posting document to Redis")
+            print(e)
 
 consume_queue()
--- namespace-project/nginx-branch/pythonServer/pythonApp.py
+++ namespace-project/nginx-branch/pythonServer/pythonApp.py
@@ -6,6 +6,34 @@ import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+
+from opentelemetry import trace
+from opentelemetry.ext import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+
+trace.set_tracer_provider(TracerProvider())
+
+# create a JaegerSpanExporter
+jaeger_exporter = jaeger.JaegerSpanExporter(
+    service_name='python-grpc-server',
+    # configure agent
+    agent_host_name='jaeger-agent.observability.svc.cluster.local',
+    agent_port=6831,
+    # optional: configure also collector
+    # collector_host_name='localhost',
+    # collector_port=14268,
+    # collector_endpoint='/api/traces?format=jaeger.thrift',
+    # username=xxxx, # optional
+    # password=xxxx, # optional
+)
+
+# Create a BatchExportSpanProcessor and add the exporter to it
+span_processor = BatchExportSpanProcessor(jaeger_exporter)
+
+# add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 redis_id = 1
 KEY_INDEX = 'index'
 
@@ -25,11 +53,13 @@ except Exception as e:
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
-        try:
-            y = json.loads(request.name)
-        except Exception as e:
-            print("Error deserializing message - %r", request.name)
-            print(e)
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span("GRPC-server") as grpc_server:  
+            try:
+                y = json.loads(request.name)
+            except Exception as e:
+                print("Error deserializing message - %r", request.name)
+                print(e)
 
         print("request deserialized -- %s", y)
         insert_mongo_db(y)
@@ -37,25 +67,29 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         return helloworld_pb2.HelloReply(message = '>>> %s' %"Caso insertado en DBs")
 
 def insert_mongo_db(data):
-    try:
-        print("***** Insertando caso a Mongo DB *****")
-        mongo_db.casos.insert_one(data)
-        print("***** Caso insertado en MongoDB: %s", data)
-    except Exception as e:
-        print("Error posting document to mongo DB")
-        print(e)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("grpc_insert_mongo_db") as grpc_insert_mongo:
+        try:
+            print("***** Insertando caso a Mongo DB *****")
+            mongo_db.casos.insert_one(data)
+            print("***** Caso insertado en MongoDB: %s", data)
+        except Exception as e:
+            print("Error posting document to mongo DB")
+            print(e)
 
 def insert_redis(data):
-    try:
-        print("***** Insertando caso a Redis *****")
-        redis_db.incr(KEY_INDEX, 1)
-        index = redis_db.get(KEY_INDEX).decode('utf-8')
-        int_index = int(index) 
-        redis_db.set(str(int_index), data)
-        print("***** Caso insertado en Redis: ", data)
-    except Exception as e:
-        print("Error posting document to Redis")
-        print(e)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("grpc_insert_redis") as grpc_insert_redis:
+        try:
+            print("***** Insertando caso a Redis *****")
+            redis_db.incr(KEY_INDEX, 1)
+            index = redis_db.get(KEY_INDEX).decode('utf-8')
+            int_index = int(index) 
+            redis_db.set(str(int_index), data)
+            print("***** Caso insertado en Redis: ", data)
+        except Exception as e:
+            print("Error posting document to Redis")
+            print(e)
 
 def server():
     print('Starting grpc server...')
